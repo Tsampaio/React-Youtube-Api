9f7d5e0a5a9d346a53087e9a9d552863ec37dffc
Telmo Sampaio <telmoasampaio@gmail.com>
2018-03-23 20:04:21 +0000

    Javascript ES6 Fat arrow function added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//Create a new component, this component will produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = function() {
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    return <div>Hi!!</div>;
+}
+
+//Take this component and add it to the page
+
+ReactDOM.render(<App />, document.querySelector('.container'));
