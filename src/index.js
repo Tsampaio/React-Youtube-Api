e119698262cd2e0b30b3b40628a966985f8f9ebf
Telmo Sampaio <telmoasampaio@gmail.com>
2018-03-24 10:00:12 +0000

    Consttructor Class and State added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY= "AIzaSyAT9jfyCgoKjtjBJoBWy8Cq23pQC184XHk";
 
 //Create a new component, this component will produce some HTML
 
 const App = () => {
-
-    return <div>Hi Telmo!!</div>;
+    return (
+    
+        <div>
+            <SearchBar />
+        </div>
+    ); 
 }
 
 //Take this component and add it to the page
